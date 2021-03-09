e8f02c138310dca963b2b128e6e72eff8e5afc47
Omair A. Khan <omair.adil.khan@gmail.com>
2021-03-09 16:20:39 -0600

    adds cron_script function

---
+++ R/cron_script.R
@@ -0,0 +1,7 @@
+# =============================================================================
+# this script will generate the full NP data set by downloading the components
+# from REDCap. It is intended to be run regularly as a cron job. This script
+# can also be invoked on demand to update the data.
+
+data.df <- generate_data_from_redcap(epoch = 1:5)
+saveRDS(data.df, file = file.path("data/complete_np_data.rds"))
