8a4d077d70d50babc11faaa8e803f80a21508acc
Omair A. Khan <omair.adil.khan@gmail.com>
2021-03-09 15:23:13 -0600

    adds code to pull and generate data from REDCap

--- R/generate_data.R
+++ R/generate_data.R
@@ -1,9 +1,15 @@
 generate_data_from_freeze <- function(epoch = 1:5) {
-  output.df <- map_df(epoch, ~ pull_map_freeze(epoch = .))
+  output.df <- map_df(epoch, ~pull_map_freeze(epoch = .))
   output.df <- output.df[output.df$age != 0, ]
   output.df <- map_df(1:nrow(output.df), ~compute_scores(output.df[., ]))
   
   return(output.df)
 }
 
-# generate_data_from_redcap
+generate_data_from_redcap <- function(epoch = 1:5) {
+  output.df <- map_df(epoch, ~pull_from_redcap(epoch = .))
+  output.df <- output.df[output.df$age != 0, ]
+  output.df <- map_df(1:nrow(output.df), ~compute_scores(output.df[., ]))
+  
+  return(output.df)
+}
---
+++ R/pull_from_redcap.R
@@ -0,0 +1,52 @@
+download_redcap_data <- function(token, api_uri = "https://redcap.vanderbilt.edu/api/") {
+  data_part1.df <- REDCapR::redcap_read_oneshot(
+    redcap_uri = api_uri,
+    token = token,
+    fields = np.var[!np.var %in% np_label.var],
+    raw_or_label = "raw",
+    verbose = FALSE
+  )$data
+  
+  data_part2.df <- REDCapR::redcap_read_oneshot(
+    redcap_uri = api_uri,
+    token = token,
+    fields = c("map_id", np_label.var),
+    raw_or_label = "label",
+    verbose = FALSE
+  )$data
+  
+  data.df <- left_join(data_part1.df, data_part2.df, by = "map_id")
+  
+  return(data.df)
+}
+
+pull_from_redcap <- function(var = np.var, epoch) {
+  if (epoch == 1) {
+    main.df <- download_redcap_data(token = redcap_tokens.df[redcap_tokens.df$epoch == 1 & redcap_tokens.df$name = "main", "token"])
+    addendum.df <- download_redcap_data(token = redcap_tokens.df[redcap_tokens.df$epoch == 1 & redcap_tokens.df$name = "addendum", "token"])
+    
+    main.df <- main.df[, np_main.var]
+    addendum.df <- addendum.df[, np_addendum.var]
+    
+    output.df <- left_join(main.df, addendum.df, by = "map_id")
+  } else if (epoch == 2) {
+    output.df <- download_redcap_data(token = redcap_tokens.df[redcap_tokens.df$epoch == 2, "token"])
+    output.df <- output.df[, var]
+  } else if (epoch == 3) {
+    output.df <- download_redcap_data(token = redcap_tokens.df[redcap_tokens.df$epoch == 3, "token"])
+    output.df <- output.df[, var]
+  } else if (epoch == 4) {
+    output.df <- download_redcap_data(token = redcap_tokens.df[redcap_tokens.df$epoch == 4, "token"])
+    output.df <- output.df[, var]
+  } else if (epoch == 5) {
+    output.df <- download_redcap_data(token = redcap_tokens.df[redcap_tokens.df$epoch == 5, "token"])
+    output.df <- output.df[, var]
+  } else {
+    stop("Epoch must be 1, 2, 3, 4, or 5.\n\n")
+  }
+  
+  output.df$np_mc_kaplan_ss <- as.character(output.df$np_mc_kaplan_ss)
+  output.df$epoch <- epoch
+  
+  return(output.df)
+}
