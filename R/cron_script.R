1baa3e601ec57dd1e872efcc393d4d750f9bbb2c
Omair A. Khan <omair.adil.khan@gmail.com>
2021-03-26 09:04:25 -0500

    undos previous commit as the two scripts are sourcing each other.

--- R/cron_script.R
+++ R/cron_script.R
@@ -3,6 +3,5 @@
 # from REDCap. It is intended to be run regularly as a cron job. This script
 # can also be invoked on demand to update the data.
 
-source("setup.R")
 data.df <- generate_data_from_redcap(epoch = 1:5)
 saveRDS(data.df, file = file.path("data/complete_np_data.rds"))
