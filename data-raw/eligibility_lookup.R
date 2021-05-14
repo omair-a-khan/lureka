df3d0663d313d189e96f69c13f2602286e1e6650
Omair A. Khan <omair.adil.khan@gmail.com>
2021-05-14 10:39:37 -0500

    initializes excel lookup code for eligibility and enrollment

---
+++ data-raw/eligibility_lookup.R
@@ -0,0 +1,36 @@
+eligibility_lookup_excel.file <- "data-raw/eligibility_lookup.xlsx"
+
+eligibility_block_design.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "block_design") %>%
+  pivot_raw_across_scale_num()
+eligibility_benton.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "benton") %>%
+  pivot_raw_across_scale_num()
+eligibility_digit_span.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "digit_span") %>%
+  pivot_raw_across_scale_num()
+eligibility_trails_a_b.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "trails_a_b") %>%
+  pivot_raw_across_scale_num()
+eligibility_trails_b_b.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "trails_b_b") %>%
+  pivot_raw_across_scale_num()
+eligibility_trails_a_w.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "trails_a_w") %>%
+  pivot_raw_across_scale_num()
+eligibility_trails_b_w.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "trails_b_w") %>%
+  pivot_raw_across_scale_num()
+eligibility_cowa_b.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "cowa_b") %>%
+  pivot_raw_across_scale_num()
+eligibility_cowa_w.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "cowa_w") %>%
+  pivot_raw_across_scale_num()
+eligibility_stroop_word_b.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "stroop_word_b") %>%
+  pivot_raw_across_scale_num()
+eligibility_stroop_color_b.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "stroop_color_b") %>%
+  pivot_raw_across_scale_num()
+eligibility_stroop_colorword_b.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "stroop_colorword_b") %>%
+  pivot_raw_across_scale_num()
+eligibility_stroop_word_w.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "stroop_word_w") %>%
+  pivot_raw_across_scale_num()
+eligibility_stroop_color_w.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "stroop_color_w") %>%
+  pivot_raw_across_scale_num()
+eligibility_stroop_colorword_w.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "stroop_colorword_w") %>%
+  pivot_raw_across_scale_num()
+eligibility_wrat_std.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "wrat_std") %>%
+  pivot_raw_across_scale_chr()
+eligibility_wrat_edu.lookup <- readxl::read_xlsx(eligibility_lookup_excel.file, sheet = "wrat_edu") %>%
+  pivot_raw_across_scale_chr()
---
+++ data-raw/enrollment_lookup.R
@@ -0,0 +1,61 @@
+enrollment_lookup_excel.file <- "data-raw/enrollment_lookup.xlsx"
+
+enrollment_tower_achievement.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "tower_achievement") %>%
+  pivot_age_across_scale_num()
+enrollment_symbol_span.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "symbol_span") %>%
+  pivot_raw_across_scale_num()
+enrollment_dkefs_trails_2.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "dkefs_trails_2") %>%
+  pivot_age_across_scale_num()
+enrollment_num_sequencing_errors.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "num_sequencing_errors") %>%
+  pivot_age_across_scale_num()
+enrollment_num_setloss_errors.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "num_setloss_errors") %>%
+  pivot_age_across_scale_num()
+enrollment_tower_achievement.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "tower_achievement") %>%
+  pivot_age_across_scale_num()
+enrollment_dkefs_trails_4.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "dkefs_trails_4") %>%
+  pivot_age_across_scale_num()
+enrollment_num_letter_sequencing_errors.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "num_letter_sequencing_errors") %>%
+  pivot_age_across_scale_num()
+enrollment_num_letter_setloss_errors.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "num_letter_setloss_errors") %>%
+  pivot_age_across_scale_num()
+enrollment_tmt_contrastdiff_ss.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "tmt_contrastdiff_ss")
+enrollment_color_naming_time.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "color_naming_time") %>%
+  pivot_age_across_scale_num()
+enrollment_word_reading_time.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "word_reading_time") %>%
+  pivot_age_across_scale_num()
+enrollment_inhibition_time.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "inhibition_time") %>%
+  pivot_age_across_scale_num()
+enrollment_colorword_comp.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "colorword_comp")
+enrollment_inhibitcolor_contrast.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "inhibitcolor_contrast")
+enrollment_color_cumpercerr.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "color_cumpercerr") %>%
+  pivot_age_across_scale_num()
+enrollment_word_cumpercerr.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "word_cumpercerr") %>%
+  pivot_age_across_scale_num()
+enrollment_inhibit_cumpercscerr.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "inhibit_cumpercscerr") %>%
+  pivot_age_across_scale_num()
+enrollment_inhibit_cumpercucerr.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "inhibit_cumpercucerr") %>%
+  pivot_age_across_scale_num()
+enrollment_inhibit_cumpercerr.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "inhibit_cumpercerr") %>%
+  pivot_age_across_scale_num()
+enrollment_tower_total_rule_violations.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "tower_total_rule_violations") %>%
+  pivot_age_across_scale_chr()
+enrollment_digit_symbol.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "digit_symbol") %>%
+  pivot_age_across_scale_num()
+
+enrollment_hvot_50_54.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "hvot_50_54") %>%
+  pivot_age_across_scale_num() %>%
+  rename(education = age)
+enrollment_hvot_55_59.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "hvot_55_59") %>%
+  pivot_age_across_scale_num() %>%
+  rename(education = age)
+enrollment_hvot_60_64.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "hvot_60_64") %>%
+  pivot_age_across_scale_num() %>%
+  rename(education = age)
+enrollment_hvot_65_110.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "hvot_65_110") %>%
+  pivot_age_across_scale_num() %>%
+  rename(education = age)
+
+enrollment_hvot_tscore.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "hvot_tscore")
+enrollment_mental_control.lookup <- readxl::read_xlsx(enrollment_lookup_excel.file, sheet = "mental_control") %>%
+  pivot_age_across_scale_num()
+
