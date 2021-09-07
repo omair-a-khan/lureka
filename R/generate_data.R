114adb136019cc487de155a417e97714df5c15b6
Omair A. Khan <omair.adil.khan@gmail.com>
2021-09-07 13:29:54 -0500

    updates code to deal with missing age

--- R/generate_data.R
+++ R/generate_data.R
@@ -9,6 +9,7 @@ generate_data_from_freeze <- function(epoch = 1:5) {
 generate_data_from_redcap <- function(epoch = 1:5) {
   output.df <- map_df(epoch, ~pull_from_redcap(epoch = .))
   output.df <- output.df[output.df$age != 0, ]
+  output.df <- output.df[!is.na(output.df$age), ]
   output.df <- map_df(1:nrow(output.df), ~compute_scores(output.df[., ]))
   
   return(output.df)
