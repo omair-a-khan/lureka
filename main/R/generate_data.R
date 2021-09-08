063d9b6196b357a9a92aa3898f42c4b5fc6e5b06
Omair A. Khan <omair.adil.khan@gmail.com>
2021-09-08 11:23:49 -0500

    removes generate_data_frmo_redcap_eligibility.R and generate_data_frmo_redcap_enrollment.R as they are now in their respective subfolders

--- main/R/generate_data.R
+++ main/R/generate_data.R
@@ -15,20 +15,3 @@ generate_data_from_redcap <- function(epoch = 1:5) {
   return(output.df)
 }
 
-generate_data_from_redcap_eligibility <- function() {
-  output.df <- pull_from_redcap_eligibility(var = np_eligibility.var)
-  output.df <- output.df[!is.na(output.df$age), ]
-  
-  output.df <- map_df(1:nrow(output.df), ~ compute_scores_eligibility(data = output.df[., ]))
-  
-  return(output.df)
-}
-
-generate_data_from_redcap_enrollment <- function() {
-  output.df <- pull_from_redcap_enrollment(var = np.var)
-  output.df <- output.df[!is.na(output.df$age), ]
-  
-  output.df <- map_df(1:nrow(output.df), ~ compute_scores_enrollment(data = output.df[., ]))
-  
-  return(output.df)
-}
